eb094b796eccc13f311b927d03d6a3e99a729c65
WooJungBae <matt.woojung@gmail.com>
2022-04-30 14:10:34 -0400

    04/30/2022 update

--- realdataanalysis/package_cgd.R
+++ realdataanalysis/package_cgd.R
@@ -31,7 +31,7 @@ library(afttest)
 #-------------------------------------------------------------
 set.seed(1)
 
-path=100
+path=200
 
 cgd_data = subset(cgd,enum==1,c(tstop,status,treat,age,steroids))
 
