c5fd71a2be7d8b58e37f34cc0e6de0d4d36b4c2e
WooJungBae <matt.woojung@gmail.com>
2022-05-13 19:10:44 -0400

    05/13/2022 update

--- SimulationStudy/check/check_result1.R
+++ SimulationStudy/check/check_result1.R
@@ -7,14 +7,14 @@ alpha = 0.05
 
 # 
 Scenario = 11
-N = 500
+N = 100
 
 # ------------------------------------------------------------------------------
 gamma_0 = 0
 txt.title = paste0("afttest","Scn",Scenario,"N",N,"gamma",gamma_0*10,"_result.txt")
 
 gamma0_result = read.table(txt.title, header = TRUE, sep = "", dec = ".", fill = T)
-gamma0_sim = nrow(gamma0_result)-sum(apply(gamma0_result, 1, function(l) (sum(is.na(l))>0)))
+gamma0_sim = nrow(gamma0_result)
 gamma0_rejectionratio = matrix(apply(gamma0_result[,-1], 2, function(l) sum(l<alpha, na.rm = T)),ncol=3)/gamma0_sim
 gamma0_rejectionratio = round(gamma0_rejectionratio,4)
 rownames(gamma0_rejectionratio) = c("mns","mnsstd","mis","misstd")
@@ -25,7 +25,7 @@ gamma_0 = 0.1
 txt.title = paste0("afttest","Scn",Scenario,"N",N,"gamma",gamma_0*10,"_result.txt")
 
 gamma1_result = read.table(txt.title, header = TRUE, sep = "", dec = ".", fill = T)
-gamma1_sim = nrow(gamma1_result)-sum(apply(gamma1_result, 1, function(l) (sum(is.na(l))>0)))
+gamma1_sim = nrow(gamma1_result)
 gamma1_rejectionratio = matrix(apply(gamma1_result[,-1], 2, function(l) sum(l<alpha, na.rm = T)),ncol=3)/gamma1_sim
 gamma1_rejectionratio = round(gamma1_rejectionratio,4)
 rownames(gamma1_rejectionratio) = c("mns","mnsstd","mis","misstd")
@@ -36,7 +36,7 @@ gamma_0 = 0.2
 txt.title = paste0("afttest","Scn",Scenario,"N",N,"gamma",gamma_0*10,"_result.txt")
 
 gamma2_result = read.table(txt.title, header = TRUE, sep = "", dec = ".", fill = T)
-gamma2_sim = nrow(gamma2_result)-sum(apply(gamma2_result, 1, function(l) (sum(is.na(l))>0)))
+gamma2_sim = nrow(gamma2_result)
 gamma2_rejectionratio = matrix(apply(gamma2_result[,-1], 2, function(l) sum(l<alpha, na.rm = T)),ncol=3)/gamma2_sim
 gamma2_rejectionratio = round(gamma2_rejectionratio,4)
 rownames(gamma2_rejectionratio) = c("mns","mnsstd","mis","misstd")
@@ -47,7 +47,7 @@ gamma_0 = 0.3
 txt.title = paste0("afttest","Scn",Scenario,"N",N,"gamma",gamma_0*10,"_result.txt")
 
 gamma3_result = read.table(txt.title, header = TRUE, sep = "", dec = ".", fill = T)
-gamma3_sim = nrow(gamma3_result)-sum(apply(gamma3_result, 1, function(l) (sum(is.na(l))>0)))
+gamma3_sim = nrow(gamma3_result)
 gamma3_rejectionratio = matrix(apply(gamma3_result[,-1], 2, function(l) sum(l<alpha, na.rm = T)),ncol=3)/gamma3_sim
 gamma3_rejectionratio = round(gamma3_rejectionratio,4)
 rownames(gamma3_rejectionratio) = c("mns","mnsstd","mis","misstd")
@@ -58,7 +58,7 @@ gamma_0 = 0.4
 txt.title = paste0("afttest","Scn",Scenario,"N",N,"gamma",gamma_0*10,"_result.txt")
 
 gamma4_result = read.table(txt.title, header = TRUE, sep = "", dec = ".", fill = T)
-gamma4_sim = nrow(gamma4_result)-sum(apply(gamma4_result, 1, function(l) (sum(is.na(l))>0)))
+gamma4_sim = nrow(gamma4_result)
 gamma4_rejectionratio = matrix(apply(gamma4_result[,-1], 2, function(l) sum(l<alpha, na.rm = T)),ncol=3)/gamma4_sim
 gamma4_rejectionratio = round(gamma4_rejectionratio,4)
 rownames(gamma4_rejectionratio) = c("mns","mnsstd","mis","misstd")
@@ -69,7 +69,7 @@ gamma_0 = 0.5
 txt.title = paste0("afttest","Scn",Scenario,"N",N,"gamma",gamma_0*10,"_result.txt")
 
 gamma5_result = read.table(txt.title, header = TRUE, sep = "", dec = ".", fill = T)
-gamma5_sim = nrow(gamma5_result)-sum(apply(gamma5_result, 1, function(l) (sum(is.na(l))>0)))
+gamma5_sim = nrow(gamma5_result)
 gamma5_rejectionratio = matrix(apply(gamma5_result[,-1], 2, function(l) sum(l<alpha, na.rm = T)),ncol=3)/gamma5_sim
 gamma5_rejectionratio = round(gamma5_rejectionratio,4)
 rownames(gamma5_rejectionratio) = c("mns","mnsstd","mis","misstd")
